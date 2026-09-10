1f1c205493f2c583a6bae9f79e1b775ee6a3ad42
raiagsan <102507201+raiagsan@users.noreply.github.com>
2026-09-10 23:27:44 +0800

    initial commit

---
+++ Assets/Game/Scripts/Input/InputManager.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.InputSystem;
+using static GameInputAction;
+
+public class InputManager : MonoBehaviour, IPlayerActions
+{
+    private GameInputAction _InputAction;
+
+    public UnityEvent<Vector2> OnMoveInput;
+    public UnityEvent<bool> OnSprintInput;
+
+    private void Awake()
+    {
+        _InputAction = new GameInputAction();
+        _InputAction.Enable();
+        _InputAction.Player.Enable();
+        _InputAction.Player.SetCallbacks(this);
+    }
+
+    public void OnInteract(InputAction.CallbackContext context)
+    {
+        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        OnMoveInput?.Invoke(context.ReadValue<Vector2>());
+    }
+
+    public void OnSprint(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            OnSprintInput?.Invoke(true);
+        }
+        if (context.canceled)
+        {
+            OnSprintInput?.Invoke(false);
+        }
+    }
+}
