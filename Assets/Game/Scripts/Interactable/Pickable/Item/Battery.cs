d2879358657aca4b07980083138e393bf6464f84
raiagsan <102507201+raiagsan@users.noreply.github.com>
2026-09-11 23:55:05 +0800

    feat: add rotation and battery system for flashlight

---
+++ Assets/Game/Scripts/Interactable/Pickable/Item/Battery.cs
@@ -0,0 +1,8 @@
+public class Battery : Item
+{
+    public override void Pickup(PlayerCharacter character)
+    {
+        base.Pickup(character);
+        character.Flashlight.RefillBatteryLevel();
+    }
+}
